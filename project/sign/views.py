73363599a4f30ba34711d6ee6b17465b6eadc66d
stas_ <dromanqs@mail.ru>
2024-11-29 23:12:50 +0500

    context в рендер шаблона

--- project/sign/views.py
+++ project/sign/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from cart.cart import Cart
+
 
 # Create your views here.
 
 
 def profile(request):
-    return render(request, 'sign/profile.html')
+    cart = Cart(request)
+    return render(request, 'sign/profile.html', context={cart: 'cart'})
