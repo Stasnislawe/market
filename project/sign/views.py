311461e22307296eea44987da7f443b98fb61604
stas_ <dromanqs@mail.ru>
2024-12-01 22:54:23 +0500

    Изменение вьюхи(замена функции на класс с context_data)

--- project/sign/views.py
+++ project/sign/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from orders.models import OrderItem, OrderItemWD
 
 from cart.cart import Cart
 
@@ -9,4 +10,14 @@ from cart.cart import Cart
 
 def profile(request):
     cart = Cart(request)
-    return render(request, 'sign/profile.html', context={cart: 'cart'})
+    orders = OrderItemWD.objects.filter(order__user=request.user.id)
+    return render(request, 'sign/profile.html', context={cart: 'cart', orders: 'orders'})
+
+
+class ProfileView(TemplateView):
+    template_name = 'sign/profile.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['cart'] = Cart(self.request)
+        kwargs['orders'] = OrderItemWD.objects.filter(order__user=self.request.user)
+        return super().get_context_data(**kwargs)
